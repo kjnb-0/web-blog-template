c75631d451ef6c36a2adcdd98c4d852a3c02c056
Krista Nielsen <kristanielsen@Kristas-MacBook-Air.local>
2021-10-05 12:00:43 -0700

    fixed helper, add css. homepage working

--- Main/controllers/dashboardRoutes.js
+++ Main/controllers/dashboardRoutes.js
@@ -97,7 +97,7 @@ router.get("/create/", withAuth, (req, res) => {
   })
     .then((dbPostData) => {
       const posts = dbPostData.map((post) => post.get({ plain: true }));
-      res.render("create-post", { posts, loggedIn: true });
+      res.render("add-post", { posts, loggedIn: true });
     })
     .catch((err) => {
       console.log(err);

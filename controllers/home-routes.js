7e3a7dded4e876ff34027172f4f3abafd161680d
Krista Nielsen <kristanielsen@Kristas-MacBook-Air.local>
2021-10-18 12:30:23 -0700

    more tweaks

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -6,8 +6,6 @@ const withAuth = require("../utils/auth");
 
 
 router.get("/", (req, res) => {
-  console.log(req.session);
-
   Post.findAll({
     attributes: ["id", "title", "created_at", "post_content"],
     include: [
